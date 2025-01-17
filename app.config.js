14bfa9085c514ded9dc742c8623f0a3e89588702
agiffachri11 <agiffachri11@gmail.com>
2025-01-17 18:39:49 +0700

    Pesan commit

--- app.config.js
+++ app.config.js
@@ -1,32 +1,32 @@
 export default {
-  expo: {
-    name: "Geometry3D",
-    slug: "geometry-mobile",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
-    splash: {
-      image: "./assets/splash-icon.png",
-      resizeMode: "contain",
-      backgroundColor: "#2B3A67",
-    },
-    assetBundlePatterns: ["**/*"],
-    ios: {
-      supportsTablet: true,
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#2B3A67",
-      },
-      package: "com.agiffachri.Geometry3D",
-      googleServicesFile: "./config/google-services.json",
-    },
-    extra: {
-      eas: {
-        projectId: "886a1f7a-3d94-4bf8-bb54-e30eef54d80c",
-      },
-    },
+  name: "geometry-mobile",
+  slug: "geometry-mobile",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#2B3A67"
   },
+  assetBundlePatterns: ["**/*"],
+  android: {
+    package: "com.agiffachri.Geometry3D",
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#2B3A67"
+    },
+    buildToolsVersion: "33.0.0",
+    compileSdkVersion: 33,
+    targetSdkVersion: 33,
+    gradle: {
+      javaHome: null
+    }
+  },
+  extra: {
+    eas: {
+      projectId: "886a1f7a-3d94-4bf8-bb54-e30eef54d80c"
+    }
+  }
 };
